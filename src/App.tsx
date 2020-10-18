c8dda37d93435b3ef4f0d9160df204961fb76d4c
Sławomir Kusiński <skwinged@gmail.com>
2020-10-18 15:35:45 +0200

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <div></div>
+        <header><h1>Digi TeX Note</h1></header>
+      </nav>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;

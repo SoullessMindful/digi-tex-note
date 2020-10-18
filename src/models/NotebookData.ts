935841a2e635c92fe1e7c05fd78633324ffb0547
Sławomir Kusiński <skwinged@gmail.com>
2020-10-18 19:07:29 +0200

    Data constructors for Notebook data structures created

--- src/models/NotebookData.ts
+++ src/models/NotebookData.ts
@@ -29,3 +29,31 @@ export const EMPTY_NOTEBOOK_DATA = {
     EMPTY_SECTION_DATA
   ]
 }
+
+export const BlockData: (obj: any) => BlockData | null
+  = (obj) => typeof(obj) === 'object' ?
+    {
+      code: obj.code ?? ''
+    } : null
+
+export const SectionData: (obj: any) => SectionData | null
+  = (obj) => typeof(obj) === 'object' ?
+    {
+      header: obj.header ?? '',
+      blocks: Array.isArray(obj.blocks) ?
+        (obj.blocks as Array<any>)
+          .map((block) => BlockData(block))
+          .filter((block) => block !== null) as BlockData[] :
+        []
+    } : null
+
+export const NotebookData: (obj: any) => NotebookData | null
+  = (obj) => typeof(obj) === 'object' ?
+    {
+      title: obj.title ?? '',
+      sections: Array.isArray(obj.sections) ?
+        (obj.sections as Array<any>)
+          .map((section) => SectionData(section))
+          .filter((section) => section !== null) as SectionData[] :
+          []
+    } : null

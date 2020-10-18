120a8bd2e3c2d43fcbb1710ebfa18414f046e3e6
Sławomir Kusiński <skwinged@gmail.com>
2020-10-18 16:21:43 +0200

    A Notebook starting with sample data shown in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Notebook } from './components/Notebook';
+import { NotebookData } from './models/NotebookData';
+
+const sampleNotebook: NotebookData = {
+  title: 'It is the title',
+  sections: [
+    {
+      header: 'Section 1',
+      blocks: [
+        {
+          code: 'Let \\( x \\in X \\).'
+        }
+      ]
+    },
+    {
+      header: 'The second section',
+      blocks: [
+        {
+          code: ''
+        },
+        {
+          code: ''
+        }
+      ]
+    }
+  ]
+}
 
 function App() {
+  const [notebookData, setNotebookData] = useState<NotebookData>(sampleNotebook)
+
   return (
     <div className="App">
       <nav>
@@ -9,7 +38,10 @@ function App() {
         <header><h1>Digi TeX Note</h1></header>
       </nav>
       <main>
-        
+        <Notebook
+          data={notebookData}
+          setData={setNotebookData}
+        />
       </main>
     </div>
   );
